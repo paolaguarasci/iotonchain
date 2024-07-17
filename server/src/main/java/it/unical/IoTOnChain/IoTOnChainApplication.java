48dd440cd9c5db9a8a2152c3bc796eb17fdb6a10
Paola Guarasci <paolaguarasci@gmail.com>
2024-07-17 20:01:28 +0200

    first commit
    Signed-off-by: Paola Guarasci <paolaguarasci@gmail.com>

---
+++ server/src/main/java/it/unical/IoTOnChain/IoTOnChainApplication.java
@@ -0,0 +1,13 @@
+package it.unical.IoTOnChain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTOnChainApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(IoTOnChainApplication.class, args);
+  }
+  
+}
---
+++ server/src/test/java/it/unical/IoTOnChain/IoTOnChainApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.IoTOnChain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTOnChainApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
