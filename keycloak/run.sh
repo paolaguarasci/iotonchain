bff3d61b161cfc5771019784c1587e1552053b1e
Paola Guarasci <paolaguarasci@gmail.com>
2024-07-21 16:15:23 +0200

    data keycloak

---
+++ keycloak/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+docker run -p 8881:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin -v ./data:/opt/keycloak/data/import quay.io/keycloak/keycloak:latest start-dev --import-realm
