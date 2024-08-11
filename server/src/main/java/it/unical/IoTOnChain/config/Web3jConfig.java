23bd1107b306577bf33c3496566e35e7c026ebc3
Paola Guarasci <paolaguarasci@gmail.com>
2024-08-11 15:56:48 +0200

    another clean commit
    Signed-off-by: Paola Guarasci <paolaguarasci@gmail.com>

--- server/src/main/java/it/unical/IoTOnChain/config/Web3jConfig.java
+++ server/src/main/java/it/unical/IoTOnChain/config/Web3jConfig.java
@@ -23,29 +23,18 @@ import java.util.Map;
 public class Web3jConfig {
   private final String polygonNet;
   private final String privateKey;
-  
   private final String contractAddress_hash;
-//  private final String contractAddress_box;
-//  private final String contractAddress_tokenv1;
-//  private final String contractAddress_tokenv2;
-  
   private Web3j web3j;
   private TransactionManager transactionManager;
   
   Web3jConfig(
     @Value("${app.chain.polygon}") String polygonNet,
     @Value("${app.chain.pvtkey}") String privateKey,
-    @Value("${app.chain.address.contract.hash}") String chash//,
-//    @Value("${app.chain.address.contract.box}") String cbox,
-//    @Value("${app.chain.address.contract.token.v1}") String ctokenv1,
-//    @Value("${app.chain.address.contract.token.v2}") String ctokenv2
+    @Value("${app.chain.address.contract.hash}") String chash
   ) {
     this.polygonNet = polygonNet;
     this.privateKey = privateKey;
     this.contractAddress_hash = chash;
-//    this.contractAddress_box = cbox;
-//    this.contractAddress_tokenv1 = ctokenv1;
-//    this.contractAddress_tokenv2 = ctokenv2;
   }
   
   @Bean
@@ -58,7 +47,6 @@ public class Web3jConfig {
   public TransactionManager getTransactionManager() throws IOException {
     Credentials credentials = Credentials.create(this.privateKey);
     String chainId = this.web3j.netVersion().send().getNetVersion();
-    // String chainId = "820001";
     return new RawTransactionManager(this.web3j, credentials, Long.parseLong(chainId));
   }
   
@@ -66,13 +54,8 @@ public class Web3jConfig {
   public Map<String, Object> registrationContracts() {
     Credentials credentials = Credentials.create(this.privateKey);
     ContractGasProvider contractGasProvider = new DefaultGasProvider();
-    
     Map<String, Object> contracts = new HashMap<>();
-    
     contracts.put("hash", Hash.load(this.contractAddress_hash, this.web3j, credentials, contractGasProvider));
-//    contracts.put("box", Box.load(this.contractAddress_box, this.web3j, credentials, contractGasProvider));
-//    contracts.put("tokenv1", Token.load(this.contractAddress_tokenv1, this.web3j, credentials, contractGasProvider));
-//    contracts.put("tokenv2", TokenV2.load(this.contractAddress_tokenv2, this.web3j, credentials, contractGasProvider));
     return contracts;
   }
   
