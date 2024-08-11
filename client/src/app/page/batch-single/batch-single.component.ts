b1d3a33597a622218c97ee86908257e42db668ed
Paola Guarasci <paolaguarasci@gmail.com>
2024-08-11 21:56:02 +0200

    pagina di info batch
    Signed-off-by: Paola Guarasci <paolaguarasci@gmail.com>

--- client/src/app/page/batch-single/batch-single.component.ts
+++ client/src/app/page/batch-single/batch-single.component.ts
@@ -6,10 +6,13 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { QRCodeModule } from 'angularx-qrcode';
 import { environment } from '../../../environments/environment';
+import { ChipsModule } from 'primeng/chips';
+import { TransferService } from '../../services/transfer.service';
+
 @Component({
   selector: 'app-batch-single',
   standalone: true,
-  imports: [CommonModule, ButtonModule, QRCodeModule],
+  imports: [CommonModule, ButtonModule, QRCodeModule, ChipsModule],
   templateUrl: './batch-single.component.html',
   styleUrl: './batch-single.component.scss'
 })
@@ -18,23 +21,24 @@ export class BatchSingleComponent implements OnInit {
   baseUrlQrData !: any;
   batch !: any;
   batchTruck !: any;
+  infobatch !: any;
   id!: any;
   isMine !: any;
   companyLogged !: any;
   qrData !: any;
+  companyOwnerName !: any;
 
-  constructor(private keycloakService: KeycloakService, private batchService: CompanyBatchService, private router: Router, private route: ActivatedRoute) {
+  constructor(    private transferService: TransferService, private keycloakService: KeycloakService, private batchService: CompanyBatchService, private router: Router, private route: ActivatedRoute) {
     this.baseUrlQrData = environment.baseUrlQrData;
-    
+    this.infobatch = [];
   }
 
   async ngOnInit() {
-await this.getProfile();
+    await this.getProfile();
     this.route.paramMap.subscribe((pm: any) => {
       this.clean();
       this.id = pm.get("id");
       this.update();
-
     })
   }
 
@@ -52,6 +56,7 @@ await this.getProfile();
     this.id = null;
     this.isMine = false;
     this.companyLogged = null;
+    this.infobatch = [];
   }
 
   update() {
@@ -62,8 +67,32 @@ await this.getProfile();
           this.batch = res;
 
           if (!this.companyLogged) {
-  await this.getProfile();
+            await this.getProfile();
           }
+
+    this.transferService.getAllByBatchId(this.batch.batchId).subscribe({
+      next: (res: any) => {
+        res.map((tx: any) => {
+          let formatDate = tx.transferDateStart;
+          let formatDateStr = ""
+          if (tx.transferDateStart) {
+            formatDate = new Date(tx.transferDateStart);
+            formatDateStr = formatDate.toLocaleString();
+          }
+          this.infobatch.push({
+            date: formatDateStr,
+            from: tx.companySenderUsername,
+            to: tx.companyRecipientUsername,
+            batchId: tx.newBatchId,
+          });
+        });
+        console.log(res);
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+    });
+
 
           this.qrData = this.baseUrlQrData + "/" +  this.companyLogged  + "/" + this.batch.batchId
         },
