ee71f300b065384153e1f4df7493ab494dab9b2a
Paola Guarasci <paolaguarasci@gmail.com>
2024-08-23 15:14:19 +0200

    ss

--- sensor/send.py
+++ sensor/send.py
@@ -8,14 +8,14 @@ url = os.getenv("DTURL")
 credential = DefaultAzureCredential()
 service_client = DigitalTwinsClient(url, credential)
 digitalTwinId = 'thermostat67'
-digitalTwinId = 'truck_barillaspa_5'
+digitalTwinId = 'truck_barillaspa_6'
 propertye = 'Temperature'
 newValue = dict();
 newValue["path"] =  '/' + propertye
 newValue["op"] = "replace"
 
 while True:
-  newValue["value"] = 123
+  newValue["value"] = 16
   service_client.update_digital_twin(digitalTwinId, [newValue])
   print(f'Invio lettura: {newValue["value"]}')
   time.sleep(1)
